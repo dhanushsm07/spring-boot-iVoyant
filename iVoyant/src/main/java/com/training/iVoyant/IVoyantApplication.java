6c518b9522fc9a4d9b6b0f3c16b254d6a7171e1c
dhanushsm07 <dhanushm@ivoyant.com>
2026-02-03 11:49:03 +0530

    Spring Boot Application Intialization

---
+++ iVoyant/src/main/java/com/training/iVoyant/IVoyantApplication.java
@@ -0,0 +1,13 @@
+package com.training.iVoyant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IVoyantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IVoyantApplication.class, args);
+	}
+
+}
---
+++ iVoyant/src/test/java/com/training/iVoyant/IVoyantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.iVoyant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IVoyantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
